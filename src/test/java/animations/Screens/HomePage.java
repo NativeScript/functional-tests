6fd79543f20069c4753622c7cea83c7d02f80a3a
vchimev <Vasil.Chimev@telerik.com>
2017-02-08 16:31:47 +0200

    Update Animation Tests
    The animation demo has all elements inside a scroll view with scrollable="false".
    https://github.com/NativeScript/animation-demo/blob/release/app/main-page.xml
    As a result, scroll() method of Appium fails to scroll successfully for Android.
    
    Call swipe() method of Appium for Android in order to navigate to the element.

--- src/test/java/animations/Screens/HomePage.java
+++ src/test/java/animations/Screens/HomePage.java
@@ -3,7 +3,11 @@ package animations.Screens;
 import functional.tests.core.basepage.BasePageExtended;
 import functional.tests.core.basetest.Context;
 import functional.tests.core.element.UIElement;
+import functional.tests.core.enums.PlatformType;
 import functional.tests.core.exceptions.AppiumException;
+import functional.tests.core.helpers.NavigationHelper;
+import io.appium.java_client.SwipeElementDirection;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 
 public class HomePage extends BasePageExtended {
@@ -28,6 +32,26 @@ public class HomePage extends BasePageExtended {
             this.log.info("Animations app home page loaded.");
         } else {
             Assert.fail("Animations app home page NOT loaded.");
+        }
+    }
+
+    public void navigateTo(String example, By locator) {
+        this.navigationManager.navigateToMainPage();
+
+        UIElement element;
+        if (this.settings.platform == PlatformType.Andorid) {
+            element = this.gestures.swipeInWindowToElement(SwipeElementDirection.DOWN, this.locators.byText(example), 10, 250, 250);
+        } else {
+            element = NavigationHelper.scrollTo(example, this.context, 5);
+        }
+
+        if (element == null) {
+            this.log.error("Example is not found");
+        } else {
+            this.navigateTo(element, example);
+        }
+        if (locator != null) {
+            this.wait.waitForVisible(locator, this.settings.shortTimeout, true);
         }
     }
 }
