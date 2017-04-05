d602396f3988dba213770adb516f0c877503e415
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2017-04-05 11:11:05 +0300

    Fix groceries login page

--- src/test/java/groceries/Screens/LoginPage.java
+++ src/test/java/groceries/Screens/LoginPage.java
@@ -2,40 +2,27 @@ package groceries.Screens;
 
 import functional.tests.core.mobile.basepage.BasePage;
 import functional.tests.core.mobile.element.UIElement;
-import functional.tests.core.exceptions.AppiumException;
 import org.openqa.selenium.By;
 
 public class LoginPage extends BasePage {
 
-    public UIElement btnLogin;
+    public final By btnLogin = this.locators.byText("Login", false, false);
 
     public LoginPage() {
         super();
     }
 
-    public By loginButtonLocator() {
-        return this.locators.byText("Login", false, false);
-    }
-
     public LoginPage loaded() {
-        this.btnLogin = this.wait.waitForVisible(loginButtonLocator());
-        if (this.btnLogin != null)
+        UIElement login = this.wait.waitForVisible(this.btnLogin);
+        if (login != null)
             this.log.info("Groceries login page loaded.");
 
         return this;
     }
 
-    public void unloaded() throws AppiumException {
-        this.wait.waitForNotVisible(loginButtonLocator(), this.settings.shortTimeout, true);
-        this.log.info("Groceries login page not visible.");
-    }
-
     public SecondaryLoginPage login() {
-        if (this.settings.platformVersion == 4.2) {
-            this.btnLogin.tap();
-        } else {
-            this.btnLogin.click();
-        }
+        UIElement login = this.wait.waitForVisible(this.btnLogin);
+        login.tap();
         SecondaryLoginPage page = new SecondaryLoginPage();
 
         return page;
