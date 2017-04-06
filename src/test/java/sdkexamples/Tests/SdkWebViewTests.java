aee13426cb76335f615f5ae33d8585f536dc3dd5
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2017-04-06 14:56:22 +0300

    Update sdkWebView test

--- src/test/java/sdkexamples/Tests/SdkWebViewTests.java
+++ src/test/java/sdkexamples/Tests/SdkWebViewTests.java
@@ -1,5 +1,6 @@
 package sdkexamples.Tests;
 
+import functional.tests.core.mobile.find.Wait;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import sdkexamples.SdkBaseTest;
@@ -18,17 +19,16 @@ public class SdkWebViewTests extends SdkBaseTest {
     @DataProvider(name = "example")
     public Object[][] data() {
         return new Object[][]{
-                {pageWebViewCode, "OK"},
-                {pageWebViewHtml, null},
+                {pageWebViewCode},
+                {pageWebViewHtml},
         };
     }
 
     @Test(dataProvider = "example")
-    public void sdkWebViewTest(String example, String btn) throws Exception {
+    public void sdkWebViewTest(String example) throws Exception {
         this.mainPage.navigateTo(example);
-        if (btn != null) {
-            this.wait.waitForVisible(this.locators.byText(btn), settings.defaultTimeout, false).tap();
-        }
+        Wait.sleep(10000);
+        this.context.getDevice().assertLogContains("WebView finished loading");
         this.log.logScreen(example);
     }
 }
