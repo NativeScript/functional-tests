3b5393bebacab2f08ea2a5c600b806d6ee6af8f4
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2017-04-10 15:45:27 +0300

    Include logic to navigate back from screen saver for iOS

--- src/test/java/sdkexamples/Tests/SdkWebViewTests.java
+++ src/test/java/sdkexamples/Tests/SdkWebViewTests.java
@@ -1,10 +1,13 @@
 package sdkexamples.Tests;
 
 import functional.tests.core.enums.PlatformType;
+import functional.tests.core.mobile.element.UIRectangle;
 import functional.tests.core.mobile.find.Wait;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import sdkexamples.SdkBaseTest;
+
+import java.awt.*;
 
 public class SdkWebViewTests extends SdkBaseTest {
 
@@ -29,10 +32,13 @@ public class SdkWebViewTests extends SdkBaseTest {
     public void sdkWebViewTest(String example) throws Exception {
         this.mainPage.navigateTo(example);
         Wait.sleep(10000);
-        if (this.settings.platform == PlatformType.Android) {
+        if (this.settings.platform == PlatformType.Android && example == pageWebViewCode) {
             this.context.getDevice().assertLogContains("WebView finished loading");
             this.log.logScreen(example);
         } else {
+            UIRectangle doneBtn = new UIRectangle(new Rectangle(17, 25, 200, 200), this.context);
+            doneBtn.tap();
+            doneBtn.tap();
             this.assertScreen(20);
         }
     }
