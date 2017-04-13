2ccf83fbd0ffb9d7387bc4e44dd122a3ef263987
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2017-04-13 11:57:39 +0300

    Fix sdkWebViewTest

--- src/test/java/sdkexamples/Tests/SdkWebViewTests.java
+++ src/test/java/sdkexamples/Tests/SdkWebViewTests.java
@@ -31,17 +31,18 @@ public class SdkWebViewTests extends SdkBaseTest {
     @Test(dataProvider = "example")
     public void sdkWebViewTest(String example) throws Exception {
         this.mainPage.navigateTo(example);
+        String msg = "WebView finished loading";
         Wait.sleep(10000);
-        if (this.settings.platform == PlatformType.Android && example == pageWebViewCode) {
-            this.context.getDevice().assertLogContains("WebView finished loading");
+        if (this.settings.platform == PlatformType.Android && this.settings.platformVersion == 4.4 && example == pageWebViewCode) {
+            this.assertScreen(this.context.getTestName() + "_" + example, 20);
+        } else if (this.settings.platform == PlatformType.Android && example == pageWebViewCode && this.settings.platformVersion != 4.4) {
+            this.context.getDevice().assertLogContains(msg);
             this.log.logScreen(example);
-        } else {
-            if (example == pageWebViewCode && this.settings.platform == PlatformType.iOS) {
-                UIRectangle doneBtn = new UIRectangle(new Rectangle(17, 25, 200, 200), this.context);
-                doneBtn.tap();
-                doneBtn.tap();
-                this.assertScreen(20);
-            }
+        } else if (example == pageWebViewCode && this.settings.platform == PlatformType.iOS) {
+            UIRectangle doneBtn = new UIRectangle(new Rectangle(17, 25, 200, 200), this.context);
+            doneBtn.tap();
+            doneBtn.tap();
+            this.assertScreen(20);
         }
     }
 }
