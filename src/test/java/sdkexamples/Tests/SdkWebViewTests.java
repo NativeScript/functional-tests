f4180993577b36b51f232568f6c8213f56cffe47
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2017-04-13 16:19:04 +0300

    Update web view test

--- src/test/java/sdkexamples/Tests/SdkWebViewTests.java
+++ src/test/java/sdkexamples/Tests/SdkWebViewTests.java
@@ -3,6 +3,8 @@ package sdkexamples.Tests;
 import functional.tests.core.enums.PlatformType;
 import functional.tests.core.mobile.element.UIRectangle;
 import functional.tests.core.mobile.find.Wait;
+import org.openqa.selenium.By;
+import org.springframework.util.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import sdkexamples.SdkBaseTest;
@@ -31,18 +33,19 @@ public class SdkWebViewTests extends SdkBaseTest {
     @Test(dataProvider = "example")
     public void sdkWebViewTest(String example) throws Exception {
         this.mainPage.navigateTo(example);
-        String msg = "WebView finished loading";
+
         Wait.sleep(10000);
-        if (this.settings.platform == PlatformType.Android && this.settings.platformVersion == 4.4 && example == pageWebViewCode) {
-            this.assertScreen(this.context.getTestName() + "_" + example, 20);
-        } else if (this.settings.platform == PlatformType.Android && example == pageWebViewCode && this.settings.platformVersion != 4.4) {
-            this.context.getDevice().assertLogContains(msg);
-            this.log.logScreen(example);
-        } else if (example == pageWebViewCode && this.settings.platform == PlatformType.iOS) {
-            UIRectangle doneBtn = new UIRectangle(new Rectangle(17, 25, 200, 200), this.context);
-            doneBtn.tap();
-            doneBtn.tap();
-            this.assertScreen(20);
+        if (example == pageWebViewCode) {
+            By locator = this.locators.labelLocator();
+            if (this.settings.platform == PlatformType.iOS) {
+                UIRectangle doneBtn = new UIRectangle(new Rectangle(17, 25, 200, 200), this.context);
+                doneBtn.tap();
+                doneBtn.tap();
+            }
+
+            Assert.isTrue(this.wait.waitForVisible(locator).getText().contains("WebView finished loading"));
         }
+
+        this.log.logScreen(example);
     }
 }
