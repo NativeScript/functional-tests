4336a6464773220f24b56b289b061524cafb92ee
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2017-04-19 14:23:55 +0300

    Increase wait time in ApplicationTests

--- src/test/java/uitests/Tests/Component/ApplicationTests.java
+++ src/test/java/uitests/Tests/Component/ApplicationTests.java
@@ -12,6 +12,7 @@ public class ApplicationTests extends MobileTest {
     public void application_01_launch_resume() throws Exception {
         if (this.settings.platform == PlatformType.Android) {
             this.app.restart();
+            Wait.sleep(3000);
             this.context.getDevice().assertLogContains("### Launched application with:");
             this.log.info("The log contains '### Launched application with:'");
 
@@ -33,7 +34,7 @@ public class ApplicationTests extends MobileTest {
         this.log.info("Run the application in background.");
         this.app.runInBackground(2);
         this.wait.waitForVisible(this.locators.byText("Run"), 15, true);
-        Wait.sleep(30);
+        Wait.sleep(3000);
         this.device.assertLogContains("#1# SuspendEvent");
         if (this.settings.platform != PlatformType.iOS && !this.settings.platformVersion.toString().startsWith("9")) {
             this.device.assertLogContains("#2# ResumeEvent");
