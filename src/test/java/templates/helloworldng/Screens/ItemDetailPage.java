6a3d65a09e03e0d29db6701f9d3aff2532a62261
vchimev <Vasil.Chimev@telerik.com>
2017-05-15 10:30:08 +0300

    Update HelloWorldNg Tests

---
+++ src/test/java/templates/helloworldng/Screens/ItemDetailPage.java
@@ -0,0 +1,21 @@
+package templates.helloworldng.Screens;
+
+import functional.tests.core.mobile.basepage.BasePage;
+import functional.tests.core.mobile.element.UIElement;
+import org.testng.Assert;
+
+public class ItemDetailPage extends BasePage {
+
+    public ItemDetailPage() {
+        super();
+    }
+
+    private UIElement title() {
+        return this.find.byText("Details");
+    }
+
+    public void loaded() {
+        Assert.assertNotNull(this.title());
+        this.log.info("HelloWorldNG item detail page loaded.");
+    }
+}
--- src/test/java/templates/helloworldng/Tests/SmokeTests.java
+++ src/test/java/templates/helloworldng/Tests/SmokeTests.java
@@ -1,26 +1,56 @@
 package templates.helloworldng.Tests;
 
 import functional.tests.core.mobile.basetest.MobileTest;
+import io.appium.java_client.SwipeElementDirection;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import templates.helloworldng.Screens.HomePage;
+import templates.helloworldng.Screens.ItemDetailPage;
 
 public class SmokeTests extends MobileTest {
 
-    @Test(groups = {"android", "ios"})
-    public void test_01_home() throws Exception {
-        HomePage homePage = new HomePage();
-        homePage.loaded();
-        this.log.logScreen("hello-world-ng");
-        // TODO(): Compare screens after design is final.
-        // this.compareScreens();
-        // this.assertImagesResults();
+    private HomePage homePage;
+    private ItemDetailPage itemDetailPage;
+    private String itemFirst = "Ter Stegen";
+    private String itemLast = "Masip";
+    private String itemToTap = "Umtiti";
+
+    @BeforeClass(alwaysRun = true)
+    public void beforeSmokeTestsClass() {
+        this.homePage = new HomePage();
+        this.itemDetailPage = new ItemDetailPage();
     }
 
     @Test(groups = {"android", "ios"})
-    public void test_02_navigation() throws Exception {
-        HomePage homePage = new HomePage();
-        homePage.loaded();
+    public void test_01_home() throws Exception {
+        this.homePage.loaded();
+        this.assertScreen();
+    }
 
-        // TODO(): Add navigation tests
+    @Test(groups = {"android", "ios"})
+    public void test_02_swipeDown() throws Exception {
+        this.homePage.loaded();
+        this.context.gestures.swipeInWindowToElement(SwipeElementDirection.DOWN, this.locators.byText(itemLast), 3, 250, 0);
+    }
+
+    @Test(groups = {"android", "ios"})
+    public void test_03_itemDetail() throws Exception {
+        this.homePage.loaded();
+        this.homePage.tapOnItem(itemToTap);
+        this.itemDetailPage.loaded();
+        // this.assertScreen();
+    }
+
+    @Test(groups = {"android", "ios"})
+    public void test_04_back() throws Exception {
+        this.itemDetailPage.loaded();
+        this.itemDetailPage.navigateBack();
+        this.homePage.loaded();
+    }
+
+    @Test(groups = {"android", "ios"})
+    public void test_05_swipeUp() throws Exception {
+        this.homePage.loaded();
+        this.context.gestures.swipeInWindowToElement(SwipeElementDirection.UP, this.locators.byText(itemFirst), 3, 250, 0);
     }
 }
