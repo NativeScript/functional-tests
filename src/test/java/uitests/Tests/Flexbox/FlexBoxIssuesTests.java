897592dd0738aafc97da2e118d76b09b23b73a29
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2017-05-18 18:11:58 +0300

    no message

--- src/test/java/uitests/Tests/Flexbox/FlexBoxIssuesTests.java
+++ src/test/java/uitests/Tests/Flexbox/FlexBoxIssuesTests.java
@@ -1,7 +1,12 @@
 package uitests.Tests.Flexbox;
 
-/**
- * Created by tsenov on 5/18/17.
- */
-public class FlexBoxIssuesTests {
+import org.testng.annotations.Test;
+
+public class FlexBoxIssuesTests extends UIFlexboxBaseTest {
+
+    @Test(groups = {"android", "ios"})
+    public void flexbox_4143() throws Exception {
+        this.pageFlexbox.navigateTo("flexbox-4143");
+        this.assertScreen(5);
+    }
 }
