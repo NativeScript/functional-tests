8b776d245bd309ccb3ffe25e27efec1d66109173
vchimev <Vasil.Chimev@telerik.com>
2017-05-29 09:51:00 +0300

    Fix SdkTabView Tests

--- src/test/java/sdkexamples/Tests/SdkTabViewTests.java
+++ src/test/java/sdkexamples/Tests/SdkTabViewTests.java
@@ -1,5 +1,6 @@
 package sdkexamples.Tests;
 
+import org.springframework.util.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import sdkexamples.SdkBaseTest;
@@ -24,8 +25,10 @@ public class SdkTabViewTests extends SdkBaseTest {
     @Test(dataProvider = "example")
     public void sdkTabViewTest(String example) throws Exception {
         this.mainPage.navigateTo(example);
-        if (example == "TabView items") {
-            this.mainPage.find.byText("OK", 3).click();
+        if (example == "Basic TabView") {
+            Assert.notNull(this.mainPage.find.byText("NativeScript"));
+        } else if (example == "TabView items") {
+            Assert.notNull(this.mainPage.find.byText("TabView item with Icon"));
         }
     }
 }
