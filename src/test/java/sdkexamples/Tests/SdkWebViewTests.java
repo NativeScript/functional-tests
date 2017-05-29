b03990e6a4b1281bcaeb9bee4b92f5e601444dfc
vchimev <Vasil.Chimev@telerik.com>
2017-05-29 11:42:59 +0300

    Update SdkWebView Tests

--- src/test/java/sdkexamples/Tests/SdkWebViewTests.java
+++ src/test/java/sdkexamples/Tests/SdkWebViewTests.java
@@ -1,15 +1,10 @@
 package sdkexamples.Tests;
 
-import functional.tests.core.enums.PlatformType;
-import functional.tests.core.mobile.element.UIRectangle;
 import functional.tests.core.mobile.find.Wait;
-import org.openqa.selenium.By;
 import org.springframework.util.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import sdkexamples.SdkBaseTest;
-
-import java.awt.*;
 
 public class SdkWebViewTests extends SdkBaseTest {
 
@@ -33,18 +28,12 @@ public class SdkWebViewTests extends SdkBaseTest {
     @Test(dataProvider = "example")
     public void sdkWebViewTest(String example) throws Exception {
         this.mainPage.navigateTo(example);
-
-        Wait.sleep(10000);
         if (example == pageWebViewCode) {
-            if (this.settings.platform == PlatformType.iOS) {
-                UIRectangle doneBtn = new UIRectangle(new Rectangle(17, 25, 200, 200), this.context);
-                doneBtn.tap();
-                doneBtn.tap();
-            }
-
-           this.wait.waitForVisible(this.locators.byText("WebView finished loading",false,false),true);
+            Wait.sleep(10000);
+            this.wait.waitForVisible(this.locators.byText("WebView finished loading of", false, false), true);
+        } else if (example == pageWebViewHtml) {
+            Assert.notNull(this.find.byText("First WebView"));
         }
-
         this.log.logScreen(example);
     }
 }
