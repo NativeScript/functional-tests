e6fb7a968cef1d3a6edf3fa7cd46c68bd1386a9b
vchimev <Vasil.Chimev@telerik.com>
2017-05-30 10:38:10 +0300

    Add copy-apps script

---
+++ copy-apps.sh
@@ -0,0 +1,4 @@
+rm -frv ./testapp/*
+cp -v /tns-dist/TestApps/Stable/Android/*.apk ./testapp/
+cp -v /tns-dist/TestApps/Stable/iOS-Xcode8/*.ipa ./testapp/
+cp -v /tns-dist/TestApps/Stable/iOS-Xcode8/*.tgz ./testapp/
