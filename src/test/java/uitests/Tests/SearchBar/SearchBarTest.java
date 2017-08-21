27aa0690b5011f5adce4e196ceed00c2a57d56bc
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2017-08-21 14:54:20 +0300

    Update search-bar tests to hide keyboard

--- src/test/java/uitests/Tests/SearchBar/SearchBarTest.java
+++ src/test/java/uitests/Tests/SearchBar/SearchBarTest.java
@@ -50,6 +50,9 @@ public class SearchBarTest extends UIBaseTests {
     @Test(groups = {"android", "ios"})
     public void search_bar() throws Exception {
         this.homePageExtended.navigateTo(this.find.byText("search-bar"));
+        this.homePageExtended.navigateBack();
+        this.app.hideKeyboard();
+
         this.assertScreen(5);
     }
 
