8c1135945ed1aa7463f5150335769b300d06d3ea
dtopuzov <dtopuzov@gmail.com>
2018-01-17 17:14:52 +0200

    fix: QSF plugins page is known issues

--- src/test/java/nativescriptdemo/Tests/SideDrawerTests.java
+++ src/test/java/nativescriptdemo/Tests/SideDrawerTests.java
@@ -57,7 +57,7 @@ public class SideDrawerTests extends QsfBaseTest {
     public void test_05_sideDrawer_verifiedPlugins() throws Exception {
         this.homePage.tapSideDrawer();
         this.sideDrawer.tapVerifiedPlugins();
-        this.browser.handle("market.nativescript.org", "ExternalLins_VerifiedPlugins");
+        this.browser.handle("nativescript.org", "ExternalLins_VerifiedPlugins");
     }
 
     @Test(groups = {"android", "browser"})
