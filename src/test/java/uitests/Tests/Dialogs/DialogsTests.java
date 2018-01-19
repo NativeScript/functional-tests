d756290d98eb7f6fe54f2a15bc0de4e3d551c82b
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2018-01-19 10:17:22 +0200

    fix: dialog tests for ios 10

--- src/test/java/uitests/Tests/Dialogs/DialogsTests.java
+++ src/test/java/uitests/Tests/Dialogs/DialogsTests.java
@@ -1,6 +1,7 @@
 package uitests.Tests.Dialogs;
 
 
+import functional.tests.core.enums.DeviceType;
 import functional.tests.core.mobile.basetest.MobileTest;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -21,10 +22,14 @@ public class DialogsTests extends MobileTest {
     public void dialogs_11_alert() throws Exception {
         this.dialogsPage.tapAlertBtn();
         this.dialogsPage.tapOkBtn();
-        String name = this.dialogsPage.label().getText();
+        if (this.checkIfLowerThan11()) {
+            assertScreen(5);
+        } else {
+            String name = this.dialogsPage.label().getText();
 
-        Assert.assertEquals(name, DialogsPage.name1);
-        this.device.assertLogContains("### Result: undefined");
+            Assert.assertEquals(name, DialogsPage.name1);
+            this.device.assertLogContains("### Result: undefined");
+        }
     }
 
     // Action
@@ -32,24 +37,36 @@ public class DialogsTests extends MobileTest {
     public void dialogs_21_actionClose() throws Exception {
         this.dialogsPage.tapActionBtn();
         this.dialogsPage.tapCloseBtn();
-        Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name0);
-        this.device.assertLogContains("### Result: Close");
+        if (this.checkIfLowerThan11()) {
+            assertScreen(5);
+        } else {
+            Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name0);
+            this.device.assertLogContains("### Result: Close");
+        }
     }
 
     @Test(groups = {"android", "ios"})
     public void dialogs_22_actionNo() throws Exception {
         this.dialogsPage.tapActionBtn();
         this.dialogsPage.tapNoBtn();
-        Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name0);
-        this.device.assertLogContains("### Result: No");
+        if (this.checkIfLowerThan11()) {
+            assertScreen(5);
+        } else {
+            Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name0);
+            this.device.assertLogContains("### Result: No");
+        }
     }
 
     @Test(groups = {"android", "ios"})
     public void dialogs_23_actionYes() throws Exception {
         this.dialogsPage.tapActionBtn();
         this.dialogsPage.tapYesBtn();
-        Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name1);
-        this.device.assertLogContains("### Result: Yes");
+        if (this.checkIfLowerThan11()) {
+            assertScreen(5);
+        } else {
+            Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name1);
+            this.device.assertLogContains("### Result: Yes");
+        }
     }
 
     // Confirm
@@ -57,24 +74,36 @@ public class DialogsTests extends MobileTest {
     public void dialogs_31_confirmIgnore() throws Exception {
         this.dialogsPage.tapConfirmBtn();
         this.dialogsPage.tapIgnoreBtn();
-        Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name0);
-        this.device.assertLogContains("### Result: undefined");
+        if (this.checkIfLowerThan11()) {
+            assertScreen(5);
+        } else {
+            Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name0);
+            this.device.assertLogContains("### Result: undefined");
+        }
     }
 
     @Test(groups = {"android", "ios"})
     public void dialogs_32_confirmNo() throws Exception {
         this.dialogsPage.tapConfirmBtn();
         this.dialogsPage.tapNoBtn();
-        Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name0);
-        this.device.assertLogContains("### Result: false");
+        if (this.checkIfLowerThan11()) {
+            assertScreen(5);
+        } else {
+            Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name0);
+            this.device.assertLogContains("### Result: false");
+        }
     }
 
     @Test(groups = {"android", "ios"})
     public void dialogs_33_confirmYes() throws Exception {
         this.dialogsPage.tapConfirmBtn();
         this.dialogsPage.tapYesBtn();
-        Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name1);
-        this.device.assertLogContains("### Result: true");
+        if (this.checkIfLowerThan11()) {
+            assertScreen(5);
+        } else {
+            Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name1);
+            this.device.assertLogContains("### Result: true");
+        }
     }
 
     // Login
@@ -82,24 +111,36 @@ public class DialogsTests extends MobileTest {
     public void dialogs_41_loginIgnore() throws Exception {
         this.dialogsPage.tapLoginBtn();
         this.dialogsPage.tapIgnoreBtn();
-        Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name0);
-        this.device.assertLogContains("### Result: undefined, UserName: John, Password: Reese");
+        if (this.checkIfLowerThan11()) {
+            assertScreen(5);
+        } else {
+            Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name0);
+            this.device.assertLogContains("### Result: undefined, UserName: John, Password: Reese");
+        }
     }
 
     @Test(groups = {"android", "ios"})
     public void dialogs_42_loginCancel() throws Exception {
         this.dialogsPage.tapLoginBtn();
         this.dialogsPage.tapCancelBtn();
-        Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name0);
-        this.device.assertLogContains("### Result: false, UserName: John, Password: Reese");
+        if (this.checkIfLowerThan11()) {
+            assertScreen(5);
+        } else {
+            Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name0);
+            this.device.assertLogContains("### Result: false, UserName: John, Password: Reese");
+        }
     }
 
     @Test(groups = {"android", "ios"})
     public void dialogs_43_loginOk() throws Exception {
         this.dialogsPage.tapLoginBtn();
         this.dialogsPage.tapOkBtn();
-        Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name1);
-        this.device.assertLogContains("### Result: true, UserName: John, Password: Reese");
+        if (this.checkIfLowerThan11()) {
+            assertScreen(5);
+        } else {
+            Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name1);
+            this.device.assertLogContains("### Result: true, UserName: John, Password: Reese");
+        }
     }
 
     // Prompt Text
@@ -107,24 +148,36 @@ public class DialogsTests extends MobileTest {
     public void dialogs_51_promptTextIgnore() throws Exception {
         this.dialogsPage.tapPromptTextBtn();
         this.dialogsPage.tapIgnoreBtn();
-        Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name0);
-        this.device.assertLogContains("### Result: undefined, Text: John Reese");
+        if (this.checkIfLowerThan11()) {
+            assertScreen(5);
+        } else {
+            Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name0);
+            this.device.assertLogContains("### Result: undefined, Text: John Reese");
+        }
     }
 
     @Test(groups = {"android", "ios"})
     public void dialogs_52_promptTextCancel() throws Exception {
         this.dialogsPage.tapPromptTextBtn();
         this.dialogsPage.tapCancelBtn();
-        Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name0);
-        this.device.assertLogContains("### Result: false, Text: John Reese");
+        if (this.checkIfLowerThan11()) {
+            assertScreen(5);
+        } else {
+            Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name0);
+            this.device.assertLogContains("### Result: false, Text: John Reese");
+        }
     }
 
     @Test(groups = {"android", "ios"})
     public void dialogs_53_promptTextOk() throws Exception {
         this.dialogsPage.tapPromptTextBtn();
         this.dialogsPage.tapOkBtn();
-        Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name1);
-        this.device.assertLogContains("### Result: true, Text: John Reese");
+        if (this.checkIfLowerThan11()) {
+            assertScreen(5);
+        } else {
+            Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name1);
+            this.device.assertLogContains("### Result: true, Text: John Reese");
+        }
     }
 
     // Prompt Pass
@@ -145,5 +198,9 @@ public class DialogsTests extends MobileTest {
         this.dialogsPage.tapOkBtn();
         Assert.assertEquals(this.dialogsPage.label().getText(), DialogsPage.name1);
         this.device.assertLogContains("### Result: true, UserName: John, Password: Reese");
+    }
+
+    private boolean checkIfLowerThan11() {
+        return this.settings.deviceType == DeviceType.iOS && this.settings.platformVersion < 11;
     }
 }
