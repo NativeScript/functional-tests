49459e073adda768488b82fa2a724b640fb6d0e0
Dimitar Topuzov <dtopuzov@gmail.com>
2018-05-14 06:01:05 -0700

    fix: cuteness tests

--- src/test/java/cuteness/Screens/DetailsPage.java
+++ src/test/java/cuteness/Screens/DetailsPage.java
@@ -11,7 +11,7 @@ public class DetailsPage extends BasePage {
     }
 
     private UIElement details() {
-        return this.wait.waitForVisible(this.locators.byText("comments"));
+        return this.wait.waitForVisible(this.locators.byText("comments", false, false));
     }
 
     public void loaded() {
