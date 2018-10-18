73242574a77f40674924f8b200c4d68ca428f0ab
Dimitar Topuzov <dtopuzov@gmail.com>
2018-10-18 03:43:23 -0700

    refactor: Dialog tests

--- src/test/java/uitests/Tests/Dialogs/DialogsTests.java
+++ src/test/java/uitests/Tests/Dialogs/DialogsTests.java
@@ -1,14 +1,18 @@
 package uitests.Tests.Dialogs;
 
+import com.oracle.tools.packager.Log;
 import functional.tests.core.mobile.basetest.MobileTest;
+import functional.tests.core.mobile.element.UIElement;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import uitests.Screens.Dialogs.DialogsPage;
 
+@SuppressWarnings("groupsTestNG")
 public class DialogsTests extends MobileTest {
 
-    protected DialogsPage dialogsPage;
+    private DialogsPage dialogsPage;
 
     @BeforeClass(alwaysRun = true)
     public void beforeDialogsTestsSuit() {
@@ -126,71 +130,114 @@ public class DialogsTests extends MobileTest {
 
 
     @Test(groups = {"android", "ios"})
-    public void dialogs_54_promptPassOk() throws Exception {
+    public void dialogs_54_promptPassOk() {
         this.dialogsPage.tapPromptPassBtn();
 
         this.dialogsPage.tapOkBtn();
-        Assert.assertTrue(this.find.byText(DialogsPage.name1,false, 2) != null);
+        Assert.assertNotNull(this.find.byText(DialogsPage.name1, false, 2));
     }
 
     // Prompt Email
     @Test(groups = {"android", "ios"})
-    public void dialogs_55_promptEmailOk() throws Exception {
+    public void dialogs_55_promptEmailOk() {
         this.dialogsPage.tapPromptEmailBtn();
 
         this.dialogsPage.tapOkBtn();
-        Assert.assertTrue(this.find.byText(DialogsPage.name1,false, 2) != null);
+        Assert.assertNotNull(this.find.byText(DialogsPage.name1, false, 2));
+    }
+    
+    @Test(groups = {"ios"})
+    public void dialogs_promptCapitalizationNone() {
+        UIElement btnShowDialog = this.find.byText("promptCapitalizationNone");
+        btnShowDialog.click();
+        Assert.assertFalse(this.isKeyboardUpperCase(), "Initial state of keyboard should be lower case.");
+        String text = "caP";
+        this.setText(text);
+        Assert.assertFalse(this.isKeyboardUpperCase(), "After capital letter state of keyboard should be reset to lower case.");
+        this.dialogsPage.tapOkBtn();
+        Assert.assertEquals(this.dialogsPage.label().getText(), text);
     }
 
-//    Cannot simulate user input
-//    @Test(groups = {"ios"})
-//    public void dialogs_promptCapitalizationNone() {
-//        UIElement btnShowDialog = this.find.byText("promptCapitalizationNone");
-//        btnShowDialog.click();
-//        String text = "prompt Capitalization None";
-//        this.setText(text);
-//        this.dialogsPage.tapOkBtn();
-//
-//        Assert.assertEquals(this.dialogsPage.label().getText(), text.toLowerCase());
-//    }
-//
-//
-//    @Test(groups = {"ios"})
-//    public void dialogs_promptCapitalizationAll() {
-//        UIElement btnShowDialog = this.find.byText("promptCapitalizationAll");
-//        btnShowDialog.click();
-//        String text = "prompt Capitalization all";
-//        this.setText(text);
-//        this.dialogsPage.tapOkBtn();
-//
-//        Assert.assertEquals(this.dialogsPage.label().getText(), text.toUpperCase());
-//    }
-//
-//
-//    @Test(groups = {"ios"})
-//    public void dialogs_promptCapitalizationSentences() throws Exception {
-//        UIElement btnShowDialog = this.find.byText("promptCapitalizationAll");
-//        btnShowDialog.click();
-//        String text = "prompt. capitalization. sentences";
-//        this.setText(text);
-//        this.dialogsPage.tapOkBtn();
-//
-//        Assert.assertEquals(this.dialogsPage.label().getText(), "Prompt. Capitalization. Sentences");
-//    }
-//
-//    @Test(groups = {"ios"})
-//    public void dialogs_promptCapitalizationWords() throws Exception {
-//        UIElement btnShowDialog = this.find.byText("promptCapitalizationWords");
-//        btnShowDialog.click();
-//        String text = "prompt Capitalization words";
-//        this.setText(text);
-//        this.dialogsPage.tapOkBtn();
-//
-//        Assert.assertEquals(this.dialogsPage.label().getText(), "Prompt Capitalization Words");
-//    }
-//
-//    private void setText(String text) {
-//        UIElement textField = this.find.byLocator(By.xpath("//XCUIElementTypeAlert[@name=\"Name\"]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeOther[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeCollectionView/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[1]/XCUIElementTypeOther/XCUIElementTypeOther"));
-//        textField.setText(text);
-//    }
+
+    @Test(groups = {"ios"})
+    public void dialogs_promptCapitalizationAll() {
+        UIElement btnShowDialog = this.find.byText("promptCapitalizationAll");
+        btnShowDialog.click();
+        Assert.assertTrue(this.isKeyboardUpperCase(), "Initial state of keyboard should be upper case.");
+        String text = "CAp";
+        this.setText(text);
+        Assert.assertTrue(this.isKeyboardUpperCase(), "After small letter state of keyboard should be reset to upper case.");
+        this.dialogsPage.tapOkBtn();
+        Assert.assertEquals(this.dialogsPage.label().getText(), text);
+    }
+
+
+    @Test(groups = {"ios"})
+    public void dialogs_promptCapitalizationSentences() {
+        UIElement btnShowDialog = this.find.byText("promptCapitalizationSentences");
+        btnShowDialog.click();
+        Assert.assertTrue(this.isKeyboardUpperCase(), "Initial state of keyboard should be upper case.");
+        String text1 = "Sen";
+        this.setText(text1);
+        Assert.assertFalse(this.isKeyboardUpperCase(), "Keyboard should not change state until dot and space.");
+        String text2 = "t. ";
+        this.setText(text2);
+        Assert.assertTrue(this.isKeyboardUpperCase(), "Keyboard should be upper case for new sentence.");
+        this.dialogsPage.tapOkBtn();
+        Assert.assertEquals(this.dialogsPage.label().getText(), text1 + text2);
+    }
+
+    @Test(groups = {"ios"})
+    public void dialogs_promptCapitalizationWords() {
+        UIElement btnShowDialog = this.find.byText("promptCapitalizationWords");
+        btnShowDialog.click();
+        Assert.assertTrue(this.isKeyboardUpperCase(), "Initial state of keyboard should be upper case.");
+        String text1 = "Wo";
+        this.setText(text1);
+        Assert.assertFalse(this.isKeyboardUpperCase(), "Keyboard should not change state until space.");
+        String text2 = "rd ";
+        this.setText(text2);
+        Assert.assertTrue(this.isKeyboardUpperCase(), "Keyboard should be upper case for new word.");
+        this.dialogsPage.tapOkBtn();
+        Assert.assertEquals(this.dialogsPage.label().getText(), text1 + text2);
+    }
+
+    private Boolean isKeyboardUpperCase() {
+        UIElement shiftKey = this.wait.waitForVisible(By.id("shift"), this.settings.shortTimeout, true);
+        String value = shiftKey.getAttribute("value");
+        return value != null;
+    }
+
+    private void setText(String text) {
+        UIElement textField = this.find.byLocator(By.xpath("//XCUIElementTypeTextField"));
+        textField.tap();
+        for (char ch : text.toCharArray()) {
+            // Handle keyboard state
+            Boolean isCharUpperCase = Character.isUpperCase(ch);
+            UIElement shiftKey = this.wait.waitForVisible(By.id("shift"), this.settings.shortTimeout, true);
+            Boolean isKeyboardUpperCase = this.isKeyboardUpperCase();
+            if ((isCharUpperCase && isKeyboardUpperCase) || (!isCharUpperCase && !isKeyboardUpperCase)) {
+                Log.info("Keyboard state is ok.");
+            } else {
+                Log.info("Switch upper/lower case state.");
+                shiftKey.tap();
+            }
+
+            // Locate key button and tap it (with some hacks for space and dot).
+            String key = Character.toString(ch);
+            if (key.equals(" ")) {
+                key = "space";
+            }
+            if (key.equals(".")) {
+                UIElement b123 = this.wait.waitForVisible(By.id("more"), this.settings.shortTimeout, false);
+                b123.tap();
+            }
+            UIElement button = this.wait.waitForVisible(By.id(key), this.settings.shortTimeout, false);
+            button.tap();
+            if (key.equals(".")) {
+                UIElement b123 = this.wait.waitForVisible(By.id("more"), this.settings.shortTimeout, false);
+                b123.tap();
+            }
+        }
+    }
 }
